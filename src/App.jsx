c2f4e835452627004358fefaae0d4210a2fa4543
retto1337 <130590815+retto1337@users.noreply.github.com>
2025-09-08 14:38:35 +0300

    155

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 
 const theme = createTheme({
   palette: {
